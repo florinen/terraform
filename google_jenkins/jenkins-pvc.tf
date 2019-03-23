9d86906bd9e91deabcd5a4a66855a2961d96e08f
florinen <florinen>
2019-03-23 12:31:19 -0500

    deleted pv

--- google_jenkins/jenkins-pvc.tf
+++ google_jenkins/jenkins-pvc.tf
@@ -1,26 +1,3 @@
-resource "kubernetes_persistent_volume" "jenkins-pv" {
-  metadata {
-    name = "terraform-jenkins"
-  }
-
-  spec {
-    capacity {
-      storage = "15Gi"
-    }
-
-    access_modes = ["ReadWriteOnce"]
-
-    persistent_volume_source {
-      gce_persistent_disk {
-        pd_name = "jenkins-pv-pd"
-      }
-      volume {
-        volume_path = "/home/jenkins-app/volume"
-     }
-    }
-  }
-}
-
 resource "kubernetes_persistent_volume_claim" "jenkins-pvc" {
   metadata {
     name = "terraform-jenkins"
@@ -36,6 +13,4 @@ resource "kubernetes_persistent_volume_claim" "jenkins-pvc" {
       }
     }
 
-    volume_name = "${kubernetes_persistent_volume.jenkins-pv.metadata.0.terraform-jenkins}"
   }
-}
